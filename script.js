8b7e1c54b987b877ded47ce70ffaa5a638f0a7ee
corpuscoletivuslab <corpuscoletivuslab@gmail.com>
2024-06-25 03:11:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Seleciona o botão pelo ID
+const applyButton = document.getElementById('applyButton');
+
+// Adiciona um evento de clique ao botão
+applyButton.addEventListener('click', function () {
+    // Redireciona para a página apply-page.html
+    window.location.href = 'apply-page.html.html';
+});
